ad88ae661e638cf82b5c76fbdab897f38dc5b2c4
sween <khansween@gmail.com>
2024-02-17 23:01:11 +0530

    solved fibonacci number

---
+++ easyQuestion/fibonacciNumber.js
@@ -0,0 +1,33 @@
+// find a number of fibonacci
+let n = 2;
+
+// example:: 1
+function fib(n) {
+  let fibSeries = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fibSeries.push(
+      fibSeries[fibSeries.length - 2] + fibSeries[fibSeries.length - 1]
+    );
+  }
+  return fibSeries[n];
+}
+
+// example::2  ------------------------------  simple way to find out
+function fab(n) {
+  let temp = [0, 1];
+  let f = 1;
+
+  if (n < 2) return (f = n);
+
+  for (let i = 1; i < n; i++) {
+    if (temp.length === n) {
+      temp.push(temp[temp.length - 2] + temp[temp.length - 1]);
+      f = temp[temp.length - 1];
+      return f;
+    }
+    temp.push(temp[temp.length - 2] + temp[temp.length - 1]);
+    f = temp[temp.length - 1];
+  }
+}
+
+console.log(fib(n));
