48f7faefac97576151a0f082f580c233ccc951a3
sween <khansween@gmail.com>
2024-03-17 15:11:09 +0530

    modifying original array with forEach loop

--- buildingJs/practice/interviewQues/filterReduce.js
+++ buildingJs/practice/interviewQues/filterReduce.js
@@ -1,18 +1,20 @@
 let students = [
-    {name:"raje", marks: 42},
-    {name:"raje", marks: 70},
-    {name:"raje", marks: 35},
-    {name:"raje", marks: 44}
-    ]
-     
-   
-let result = students.map((stu) => {
-   if(stu.marks < 60){
-     stu.marks += 20
-   }
-   return stu
-})
-.filter((stu) => stu.marks > 60)
-.reduce((acc, curr) => acc+curr.marks, 0)
+  { name: "raje", marks: 42 },
+  { name: "raje", marks: 70 },
+  { name: "raje", marks: 35 },
+  { name: "raje", marks: 44 },
+];
 
+students.forEach((stu, i) => (students[i].age = 18));         //modifying original array with the help of forEach loop
+
+
+let result = students
+  .map((stu) => {
+    if (stu.marks < 60) {
+      stu.marks += 20;
+    }
+    return stu;
+  }).filter((stu) => stu.marks > 60).reduce((acc, curr) => acc + curr.marks, 0);
+
+  
 console.log(result);
